84f059b06d1fac3c0c5e2b9da161dc9bb740afad
prewk <oskar.thornblad@gmail.com>
2014-06-03 20:23:52 +0200

    Added some tests

--- tests/UniqueNodeParserTest.php
+++ tests/UniqueNodeParserTest.php
@@ -8,7 +8,7 @@ class UniqueNodeParserTest extends PHPUnit_Framework_TestCase
 {
     public function testLargeSimpleXml()
     {
-        $nodeNo = 100000;
+        $nodeNo = 10000;
 
         $simpleBlueprint = simplexml_load_file(__dir__ . "/simpleBlueprint.xml");
         $xmlFaker = new \Prewk\XmlFaker($simpleBlueprint);
@@ -16,15 +16,14 @@ class UniqueNodeParserTest extends PHPUnit_Framework_TestCase
         $tmpFile = tempnam("/tmp", "xml-string-streamer-test");
 
         $xmlFaker->asFile($tmpFile, \Prewk\XmlFaker::NODE_COUNT_RESTRICTION_MODE, $nodeNo);
-
         $memoryUsageBefore = memory_get_usage(true);
-        $streamProvider = new Stream\File($tmpFile, 100);
+        $provider = new Stream\File($tmpFile, 100);
 
         $counter = 0;
         $parser = new Parser\UniqueNode(array(
             "uniqueNode" => "node",
         ));
-        $streamer = new XmlStringStreamer($parser, $streamProvider);
+        $streamer = new XmlStringStreamer($parser, $provider);
 
         while ($node = $streamer->getNode()) {
             $counter++;
@@ -36,5 +35,29 @@ class UniqueNodeParserTest extends PHPUnit_Framework_TestCase
         $this->assertLessThan(500 * 1024, $memoryUsageAfter - $memoryUsageBefore, "Memory usage should not go higher than 500 KiB");
 
         unlink($tmpFile);
+    }
+
+    public function testPubmedXml()
+    {
+        $provider = new Stream\File(__dir__ . "/pubmed-example.xml", 100);
+
+        $counter = 0;
+        $parser = new Parser\UniqueNode(array(
+            "uniqueNode" => "PubmedArticle",
+        ));
+        $streamer = new XmlStringStreamer($parser, $provider);
+
+        $expectedPMIDs = array("24531174", "24529294", "24449586");
+        $foundPMIDs = array();
+
+        while ($node = $streamer->getNode()) {
+            $xmlNode = simplexml_load_string($node);
+            $foundPMIDs[] = (string)$xmlNode->MedlineCitation->PMID;
+
+            $counter++;
+        }
+
+        $this->assertEquals(3, $counter, "There should be exactly 3 nodes captured");
+        $this->assertEquals($expectedPMIDs, $foundPMIDs, "The PMID nodes should be as expected");
     }
 }
