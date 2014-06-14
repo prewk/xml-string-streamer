4ae7c21ce073faa9cf51ba15f1eb2280ab48558d
prewk <oskar.thornblad@gmail.com>
2014-06-14 16:00:36 +0200

    Special elements test

--- tests/unit/XmlStringStreamer/Parser/StringWalkerTest.php
+++ tests/unit/XmlStringStreamer/Parser/StringWalkerTest.php
@@ -143,4 +143,63 @@ eot;
             "When no nodes are left, false should be returned"
         );
     }
+
+    public function test_special_elements()
+    {
+        $node1 = <<<eot
+            <child>
+                <!-- An XML comment -->
+                <foo baz="attribute">Lorem</foo>
+                <bar>Ipsum</bar>
+                <index>1</index>
+            </child>
+eot;
+        $node2 = <<<eot
+            <child>
+                <foo baz="attribute">Lorem</foo>
+                <bar>Ipsum</bar>
+                <index>2</index>
+            </child>
+eot;
+        $node3 = <<<eot
+            <child>
+                <foo baz="attribute">Lorem</foo>
+                <bar>Ipsum</bar>
+                <index>3</index>
+            </child>
+eot;
+        $xml = <<<eot
+            <?xml version="1.0"?>
+            <!DOCTYPE foo bar baz>
+            <root>
+                $node1
+                $node2
+                $node3
+            </root>
+eot;
+    
+        $stream = $this->getStreamMock($xml, 50);
+        
+        $parser = new StringWalker();
+        
+        $this->assertEquals(
+            trim($node1),
+            trim($parser->getNodeFrom($stream)),
+            "Node 1 should be obtained on the first getNodeFrom"
+        );
+        $this->assertEquals(
+            trim($node2),
+            trim($parser->getNodeFrom($stream)),
+            "Node 2 should be obtained on the first getNodeFrom"
+        );
+        $this->assertEquals(
+            trim($node3),
+            trim($parser->getNodeFrom($stream)),
+            "Node 3 should be obtained on the first getNodeFrom"
+        );
+        $this->assertFalse(
+            false,
+            "When no nodes are left, false should be returned"
+        );
+    }
 }
