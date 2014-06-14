d5c28932bfdd76e7dcc33e93eed6308336e888c5
prewk <oskar.thornblad@gmail.com>
2014-06-14 16:10:00 +0200

    GT in comments and CDATA

--- tests/unit/XmlStringStreamer/Parser/StringWalkerTest.php
+++ tests/unit/XmlStringStreamer/Parser/StringWalkerTest.php
@@ -203,4 +203,71 @@ eot;
             "When no nodes are left, false should be returned"
         );
     }
+
+    public function test_special_elements_with_GT()
+    {
+        $node1 = <<<eot
+            <child>
+                <!--
+                    An <XML> comment with > <chars> 
+                -->
+                <foo baz="attribute">Lorem</foo>
+                <bar>Ipsum</bar>
+                <index>1</index>
+            </child>
+eot;
+        $node2 = <<<eot
+            <child>
+                <foo baz="attribute">Lorem</foo>
+                <![CDATA[
+                    A CDATA element with > chars
+                    >>><>
+                ]]>
+                <bar>Ipsum</bar>
+                <index>2</index>
+            </child>
+eot;
+        $node3 = <<<eot
+            <child>
+                <foo baz="attribute">Lorem</foo>
+                <bar>Ipsum</bar>
+                <index>3</index>
+            </child>
+eot;
+        $xml = <<<eot
+            <?xml version="1.0"?>
+            <!DOCTYPE foo bar baz>
+            <root>
+                $node1
+                $node2
+                $node3
+            </root>
+eot;
+    
+        $stream = $this->getStreamMock($xml, 50);
+        
+        $parser = new StringWalker(array(
+            "expectGT" => true,
+        ));
+        
+        $this->assertEquals(
+            trim($node1),
+            trim($parser->getNodeFrom($stream)),
+            "Node 1 should be obtained on the first getNodeFrom"
+        );
+        $this->assertEquals(
+            trim($node2),
+            trim($parser->getNodeFrom($stream)),
+            "Node 2 should be obtained on the first getNodeFrom"
+        );
+        $this->assertEquals(
+            trim($node3),
+            trim($parser->getNodeFrom($stream)),
+            "Node 3 should be obtained on the first getNodeFrom"
+        );
+        $this->assertFalse(
+            false,
+            "When no nodes are left, false should be returned"
+        );
+    }
 }
