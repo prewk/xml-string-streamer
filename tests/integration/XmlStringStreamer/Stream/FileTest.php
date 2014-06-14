d9a8c991018d079a7e381c12287d0e4ec81f9f2c
prewk <oskar.thornblad@gmail.com>
2014-06-14 13:47:59 +0200

    File stream integration tests

---
+++ tests/integration/XmlStringStreamer/Stream/FileTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Prewk\XmlStringStreamer\Stream;
+
+use \PHPUnit_Framework_TestCase;
+use \Mockery;
+
+class FileTest extends PHPUnit_Framework_TestCase
+{
+    public function test_stream_a_file_by_path()
+    {
+        $chunk1 = "1234567890";
+        $chunk2 = "abcdefghij";
+        $bufferSize = 10;
+        $full = $chunk1 . $chunk2;
+
+        $tmpFile = tempnam(sys_get_temp_dir(), "xmlss-phpunit");
+        file_put_contents($tmpFile, $full);
+
+        $stream = new File($tmpFile, $bufferSize);
+
+        $this->assertEquals($stream->getChunk(), $chunk1, "First chunk received from the stream should be as expected");
+        $this->assertEquals($stream->getChunk(), $chunk2, "Second chunk received from the stream should be as expected");
+        $this->assertEquals($stream->getChunk(), false, "Third chunk received from the stream should be false");
+    }
+
+    public function test_stream_a_file_by_handle()
+    {
+        $chunk1 = "1234567890";
+        $chunk2 = "abcdefghij";
+        $bufferSize = 10;
+        $full = $chunk1 . $chunk2;
+
+        $tmpFile = tempnam(sys_get_temp_dir(), "xmlss-phpunit");
+        file_put_contents($tmpFile, $full);
+
+        $stream = new File(fopen($tmpFile, "r"), $bufferSize);
+
+        $this->assertEquals($stream->getChunk(), $chunk1, "First chunk received from the stream should be as expected");
+        $this->assertEquals($stream->getChunk(), $chunk2, "Second chunk received from the stream should be as expected");
+        $this->assertEquals($stream->getChunk(), false, "Third chunk received from the stream should be false");
+    }
+}
