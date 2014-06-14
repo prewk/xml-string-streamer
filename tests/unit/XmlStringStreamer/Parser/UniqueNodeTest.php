f092c896601831ba0d2c08b04ef0fbd6f04897fa
prewk <oskar.thornblad@gmail.com>
2014-06-14 15:53:59 +0200

    Unit test for the UniqueNode parser

---
+++ tests/unit/XmlStringStreamer/Parser/UniqueNodeTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Prewk\XmlStringStreamer\Parser;
+
+use \PHPUnit_Framework_TestCase;
+use \Mockery;
+
+class UniqueNodeTest extends PHPUnit_Framework_TestCase
+{
+    private function getStreamMock($fullString, $bufferSize)
+    {
+        $defaultXMLLength = strlen($fullString);
+
+        $stream = Mockery::mock("\\Prewk\\XmlStringStreamer\\StreamInterface");
+        
+        // Mock a stream
+        for ($i = 0; $i < $defaultXMLLength; $i += $bufferSize) {
+            $stream->shouldReceive("getChunk")
+                   ->once()
+                   ->andReturn(substr($fullString, $i, $bufferSize));
+        }
+        $stream->shouldReceive("getChunk")
+               ->andReturn(false);
+
+        return $stream;
+    }
+
+    public function test_uniqueNode_setting()
+    {
+        $node1 = <<<eot
+            <child>
+                <foo baz="attribute">Lorem</foo>
+                <bar>Ipsum</bar>
+                <index>1</index>
+            </child>
+eot;
+        $node2 = <<<eot
+            <child>
+                <foo baz="attribute">Lorem</foo>
+                <bar>Ipsum</bar>
+                <index>2</index>
+            </child>
+eot;
+        $node3 = <<<eot
+            <child>
+                <foo baz="attribute">Lorem</foo>
+                <bar>Ipsum</bar>
+                <index>3</index>
+            </child>
+eot;
+        $xml = <<<eot
+            <?xml version="1.0"?>
+            <root>
+                $node1
+                $node2
+                $node3
+            </root>
+eot;
+    
+        $stream = $this->getStreamMock($xml, 50);
+        
+        $parser = new UniqueNode(array(
+            "uniqueNode" => "child"
+        ));
+        
+        $this->assertEquals(
+            trim($node1),
+            trim($parser->getNodeFrom($stream)),
+            "Node 1 should be obtained on the first getNodeFrom"
+        );
+        $this->assertEquals(
+            trim($node2),
+            trim($parser->getNodeFrom($stream)),
+            "Node 2 should be obtained on the first getNodeFrom"
+        );
+        $this->assertEquals(
+            trim($node3),
+            trim($parser->getNodeFrom($stream)),
+            "Node 3 should be obtained on the first getNodeFrom"
+        );
+        $this->assertFalse(
+            false,
+            "When no nodes are left, false should be returned"
+        );
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function test_requires_uniqueNode_setting()
+    {
+        $parser = new UniqueNode;
+    }
+}
