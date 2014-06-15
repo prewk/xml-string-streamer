69e888343fa82fba7297a9f0115b56924c7a3e68
prewk <oskar.thornblad@gmail.com>
2014-06-15 14:02:43 +0200

    Added self-closing element test for StringWalker

--- tests/unit/XmlStringStreamer/Parser/StringWalkerTest.php
+++ tests/unit/XmlStringStreamer/Parser/StringWalkerTest.php
@@ -270,4 +270,62 @@ eot;
             "When no nodes are left, false should be returned"
         );
     }
+
+
+    public function test_self_closing_elements()
+    {
+        $node1 = <<<eot
+            <child>
+                <foo baz="attribute" />
+                <bar/>
+                <index>1</index>
+            </child>
+eot;
+        $node2 = <<<eot
+            <child>
+                <foo baz="attribute" />
+                <bar/>
+                <index>2</index>
+            </child>
+eot;
+        $node3 = <<<eot
+            <child>
+                <foo baz="attribute" />
+                <bar/>
+                <index>3</index>
+            </child>
+eot;
+        $xml = <<<eot
+            <?xml version="1.0"?>
+            <root>
+                $node1
+                $node2
+                $node3
+            </root>
+eot;
+    
+        $stream = $this->getStreamMock($xml, 50);
+        
+        $parser = new StringWalker();
+        
+        $this->assertEquals(
+            trim($node1),
+            trim($parser->getNodeFrom($stream)),
+            "Node 1 should be obtained on the first getNodeFrom"
+        );
+        $this->assertEquals(
+            trim($node2),
+            trim($parser->getNodeFrom($stream)),
+            "Node 2 should be obtained on the first getNodeFrom"
+        );
+        $this->assertEquals(
+            trim($node3),
+            trim($parser->getNodeFrom($stream)),
+            "Node 3 should be obtained on the first getNodeFrom"
+        );
+        $this->assertFalse(
+            false,
+            "When no nodes are left, false should be returned"
+        );
+    }
 }
