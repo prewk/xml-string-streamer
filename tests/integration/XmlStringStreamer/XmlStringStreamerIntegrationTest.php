2ddc319c56a307628b8453d8bd7d1fd829959384
Oskar Thornblad <oskar.thornblad@gmail.com>
2014-09-26 23:14:36 +0200

    Added failing test

--- tests/integration/XmlStringStreamer/XmlStringStreamerIntegrationTest.php
+++ tests/integration/XmlStringStreamer/XmlStringStreamerIntegrationTest.php
@@ -80,4 +80,28 @@ class XmlStringStreamerIntegrationTest extends PHPUnit_Framework_TestCase
         
         $this->assertEquals($expectedOrphaNumbers, $foundOrphaNumbers, "The OrphaNumber nodes should be as expected");
     }
+
+    public function test_uniqueNodeParser_with_file_shorter_than_buffer()
+    {
+        $file = __dir__ . "/../../xml/short.xml";
+
+        $stream = new XmlStringStreamer\Stream\File($file, 1024);
+        $parser = new XmlStringStreamer\Parser\UniqueNode(array(
+            "uniqueNode" => "capture"
+        ));
+        $streamer = new XmlStringStreamer($parser, $stream);
+
+        $expectedNodes = array(
+            "foo",
+            "bar",
+        );
+
+        $foundNodes = array();
+        while ($node = $streamer->getNode()) {
+            $xmlNode = simplexml_load_string($node);
+            $foundNodes[] = (string)$xmlNode->node;
+        }
+
+        $this->assertEquals($expectedNodes, $foundNodes, "The found nodes should equal the expected nodes");
+    }
 }
