b02155e8b9f54ab175427f3f8bdfc2cb6dc74627
BlackbitNeueMedien <jan.walther@blackbit.de>
2020-06-17 13:33:32 +0200

    Do not close stream at end of file

--- src/XmlStringStreamer/Stream/File.php
+++ src/XmlStringStreamer/Stream/File.php
@@ -33,6 +33,12 @@ class File implements StreamInterface
         $this->chunkSize = $chunkSize;
         $this->chunkCallback = $chunkCallback;
     }
+    
+    public function __destruct() {
+        if (is_resource($this->handle)) {
+            fclose($this->handle);
+        }
+    }
 
     public function getChunk()
     {
@@ -46,7 +52,6 @@ class File implements StreamInterface
             
             return $buffer;
         } else if (is_resource($this->handle)) {
-            fclose($this->handle);
             return false;
         } else {
             return false;
