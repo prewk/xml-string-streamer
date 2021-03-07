8bfccd2b3e899858d3f4fa33123a8c3b0f721be0
Kyle Tucker <kyleatucker@gmail.com>
2021-03-07 11:34:57 -0500

    Add wrapper support to File stream (#76)
    * Add wrapper handling to File stream
    
    * Add wrapper check to FileIntegrationTest
    
    * Update FileIntegrationTest with URL wrapper test
    
    * Use faster and more evergreen URL for stream test
    
    * Use faster and more evergreen URL for stream test
    
    * Remove forgotten code
    
    * Remove noop line

--- tests/integration/XmlStringStreamer/Stream/FileIntegrationTest.php
+++ tests/integration/XmlStringStreamer/Stream/FileIntegrationTest.php
@@ -58,6 +58,46 @@ class FileIntegrationTest extends TestCase
         $this->assertEquals($callbackCount, $chunkCount, "Chunk callback count should be the same as getChunk count");
     }
 
+    public function test_compressed_file()
+    {
+        if (!extension_loaded("zlib")) {
+            $this->markTestSkipped("zlib extension is not installed");
+        }
+
+        $chunk1 = "1234567890";
+        $chunk2 = "abcdefghij";
+        $bufferSize = 10;
+        $full = $chunk1 . $chunk2;
+
+        $tmpFile = tempnam(sys_get_temp_dir(), "xmlss-phpunit");
+        $wp = fopen("compress.zlib://$tmpFile", "wb");
+        fwrite($wp, $full, $bufferSize);
+        fclose($wp);
+
+        file_put_contents($tmpFile, $full);
+
+        $stream = new File("compress.zlib://$tmpFile", $bufferSize);
+
+        $this->assertEquals($stream->getChunk(), $chunk1, "First chunk received from the stream should be as expected");
+        $this->assertEquals($stream->getChunk(), $chunk2, "Second chunk received from the stream should be as expected");
+        $this->assertEquals($stream->getChunk(), false, "Third chunk received from the stream should be false");
+    }
+
+    public function test_remote_stream()
+    {
+        if (ini_get("allow_url_fopen") !== "1") {
+            $this->markTestSkipped("allow_url_fopen is disabled");
+        }
+
+        $chunk1 = "<?xml version=\"1.0\" encoding=\"iso-8859-1\" ?>\n<!DOCTYPE html PUBLIC";
+        $bufferSize = 66;
+
+        $url = "http://www.w3.org/TR/2001/REC-xml-c14n-20010315";
+        $stream = new File($url, $bufferSize);
+
+        $this->assertEquals($chunk1, $stream->getChunk(), "First chunk received from the stream should be as expected");
+    }
+
     public function test_rewind()
     {
         $chunk1 = "1234567890";
